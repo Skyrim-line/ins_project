885c6301b93111d8bbdc93d9a7ee965af6a0da60
Simin Wu <76167009+Skyrim-line@users.noreply.github.com>
2024-06-17 00:22:26 +1000

    completed has profile module

--- INS-CLONE/frontend/src/components/Profile/ProfileInfo.jsx
+++ INS-CLONE/frontend/src/components/Profile/ProfileInfo.jsx
@@ -1,18 +1,28 @@
 import { InfoContainer } from "./Profile.styles";
 import { useParams } from "react-router-dom";
-// import { initialState as profileData } from "../../Redux/ProfileData";
-// import { initialState as postData } from "../../Redux/PostData";
+import { initialState as profileData } from "../../Redux/ProfileData";
+import { initialState as postData } from "../../Redux/PostData";
 
 const ProfileInfo = () => {
   const { id } = useParams();
   console.log("id", id);
   return (
     <>
+      {profileData[id] ? (
+        <InfoContainer>Has Profile</InfoContainer>
+      ) : (
+        <InfoContainer>
+          <h2>
+            Sorry, User with id <span>{id}</span> Does not Exitst!
+          </h2>
+        </InfoContainer>
+      )}
+      {/*       
       <InfoContainer>
         <h2>
           Sorry, User with id <span>{id}</span> Does not Exitst!
         </h2>
-      </InfoContainer>
+      </InfoContainer> */}
     </>
   );
 };
