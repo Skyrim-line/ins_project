5631c28a354d2f985199910000d9560dbcbf7dc3
Simin Wu <76167009+Skyrim-line@users.noreply.github.com>
2024-06-24 01:04:29 +1000

    Finished 4-19

--- INS-CLONE/frontend/src/components/Profile/ProfileInfo.jsx
+++ INS-CLONE/frontend/src/components/Profile/ProfileInfo.jsx
@@ -1,15 +1,51 @@
-import { InfoContainer } from "./Profile.styles";
+import { InfoContainer, Info, Status, Bio } from "./Profile.styles";
 import { useParams } from "react-router-dom";
 import { initialState as profileData } from "../../Redux/ProfileData";
 import { initialState as postData } from "../../Redux/PostData";
+import CheckCircle from "@mui/icons-material/CheckCircle";
 
 const ProfileInfo = () => {
   const { id } = useParams();
   console.log("id", id);
+  {
+    /* 定义posts使其能在个人主业上进行显示具体Posts的数量 */
+  }
+  let filteredPosts = postData.filter((post) => {
+    return post.userID === id;
+  });
   return (
     <>
       {profileData[id] ? (
-        <InfoContainer>Has Profile</InfoContainer>
+        <InfoContainer>
+          {/* 导入图片，补全图片的样式style.js设置图片样式,每个模块在style.js里面已经被定义过 */}
+          <img src={profileData[id].profilePic} alt="profile picture" />
+          <Info>
+            <p className="owner-ID">
+              {profileData[id].userID}
+              {profileData[id].verified ? (
+                <CheckCircle className="verified" />
+              ) : null}
+            </p>
+            <Status>
+              <p>
+                <strong>{filteredPosts.length}</strong> Posts
+              </p>
+              <p>
+                <strong>{profileData[id].followers}</strong> Followers
+              </p>
+              <p>
+                <strong>{profileData[id].followers}</strong> Following
+              </p>
+            </Status>
+            <Bio>
+              <p className="name">
+                <strong>{profileData[id].name}</strong>
+              </p>
+              <p className="category">{profileData[id].category}</p>
+              <p className="bio">{profileData[id].bio}</p>
+            </Bio>
+          </Info>
+        </InfoContainer>
       ) : (
         <InfoContainer>
           <h2>
